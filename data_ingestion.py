516ec3efc2839096cba50d6c180202cac9b2acce
abhinayapravallika <abhiramena@gmail.com>
2026-06-22 17:31:19 +0530

    Day 1:Data ingestion complete

---
+++ data_ingestion.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+DATA_PATH = "data/raw"
+
+csv_files = [f for f in os.listdir(DATA_PATH) if f.endswith(".csv")]
+
+for file in csv_files:
+    print("\n" + "="*50)
+    print(f"FILE: {file}")
+
+    df = pd.read_csv(os.path.join(DATA_PATH, file))
+
+    print("\nShape:")
+    print(df.shape)
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print("\nFirst 5 Rows:")
+    print(df.head())
+
+    print("\nMissing Values:")
+    print(df.isnull().sum())
