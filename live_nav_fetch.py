1fee08ff5505fd5a1d5e06b2e426f5b102e28292
abhinayapravallika <abhiramena@gmail.com>
2026-06-23 18:41:38 +0530

    Day 1: Data ingestion complete

--- live_nav_fetch.py
+++ live_nav_fetch.py
@@ -2,11 +2,16 @@ import requests
 import pandas as pd
 
 schemes = {
+    "HDFC_Top100": 125497,
     "SBI_Bluechip":119551,
     "ICICI_Bluechip":120503,
     "Nippon_LargeCap":118632,
     "Axis_Bluechip":119092,
-    "Kotak_Bluechip":120841
+    "Kotak_Bluechip":120841,
+    "UTI_Flexi_Cap": 120716,
+    "Mirae_Asset_Large_Cap": 118834,
+    "DSP_Top100_Equity": 118825,
+    "ABSL_Frontline_Equity": 119533
 }
 
 for name, code in schemes.items():
